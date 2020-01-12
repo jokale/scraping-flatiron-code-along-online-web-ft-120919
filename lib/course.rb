c6156175e6fdde16952f404263b37d3e8c79538a
Joanna E K <kalemajoanna@gmail.com>
2020-01-12 11:51:38 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,25 @@
-
+class Course 
+  attr_accessor :title, :schedule, :description
+  # def title= title 
+  #   @title= title
+  # end 
+  
+  # def title
+  #   @title
+  # end 
+    @@all = []
+ 
+  def initialize
+    @@all << self
+  end
+  
+  def self.all 
+    @@all 
+  end 
+  
+    def self.reset_all
+    @@all.clear
+  end
+  
+end 
 
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,6 +16,18 @@ class Scraper
     end
   end
   
+  def get_page
+    html = open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
+    Nokogiri::HTML(html)
+
+    end 
+    
+    def get_courses
+      
+     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+     doc.css(". ")
+    end 
+  
 end
 
 
